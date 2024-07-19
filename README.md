6506d23509b4f1289221582842c8ef4d1ae3e1be
Jesse Wei <jesseweijapan@gmail.com>
2024-07-19 22:03:29 +0900

    build ai-powered app to help streamline IELTS speaking test preparation workflow

---
+++ README.md
@@ -0,0 +1,31 @@
+# AI-powered Speaking Test Prep Assistant App
+
+It transcribes the audio record of your answer, revises and enhances it and generates a text file containing both your answer and AI-enhanced answer.
+I built it to streamline my IELTS speaking test preparation workflow.
+
+## How to Use
+
+### Step 1: Clone this repo
+```
+git clone git@github.com:mrjwei/medium-toc-generator.git
+cd medium-toc-generator
+```
+
+### Step 2: Create venv and install dependencies
+```
+python3 -m venv app-env
+source app-env/bin/activate
+pip install -r requirements.txt
+```
+
+### Step 3: Run programme
+```
+python main.py
+```
+This will open a GUI.
+
+### Step 4: Get the toc
+Copy your **published** posts' urls and paste them into the GUI's inputs, one input for a url.
+To add more input fields, hit *Add*.
+Then, hit *Generate* and the generated TOCs will be opened in your browser, one tab for a TOC, and they are ready to be copied.
+Just go over each of the tabs, copying and pasted the TOC into its corresponding post.
---
+++ main.py
@@ -0,0 +1,109 @@
+from openai import OpenAI
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import filedialog
+from tkinter import messagebox
+from pathlib import Path
+
+class SpeakingAssistant(Frame):
+  def __init__(self, root):
+    super().__init__(root)
+    self.root = root
+    self.root.title = 'Speaking Assistant'
+    self.filepath = None
+    self.original_answer = None
+    self.revised_answers = None
+
+    self.main_frame = Frame(self.root)
+    self.main_frame.grid(padx=20, pady=20, sticky='we')
+    self.main_frame.columnconfigure(0, minsize=100, weight=1)
+    self.main_frame.columnconfigure(1, minsize=100, weight=1)
+    self.main_frame.columnconfigure(2, minsize=100, weight=1)
+    self.main_frame.columnconfigure(3, minsize=100, weight=1)
+
+    Label(self.main_frame, text='Question:').grid(row=0, column=0, sticky=W, padx=5)
+
+    self.question_entry = Entry(self.main_frame)
+    self.question_entry.grid(row=1, column=0, columnspan=4, sticky='we', padx=5)
+
+    self.filepath_btn = Button(self.main_frame, text='Select Audio File', command=self.open_file_browser)
+    self.filepath_btn.grid(row=2, column=0, columnspan=4, sticky='we', padx=5)
+
+    self.filepath_label = Label(self.main_frame, text='')
+    self.filepath_label.grid(row=3, column=0, columnspan=4, padx=5)
+
+    self.gen_btn = Button(self.main_frame, text='Generate Text', command=self.generate_text)
+    self.gen_btn.grid(row=4, column=0, columnspan=2, sticky='we', padx=5)
+
+    self.quit_btn = Button(self.main_frame, text='Quit', command=lambda: self.root.quit())
+    self.quit_btn.grid(row=4, column=2, columnspan=2, sticky='we', padx=5)
+
+    self.result_label = Label(self.main_frame, text='')
+    self.result_label.grid(row=5, column=0, columnspan=4)
+
+  def open_file_browser(self):
+    self.filepath = filedialog.askopenfilename()
+    if not self.filepath:
+      return
+    self.filepath_label.configure(text=self.filepath)
+
+  def generate_text(self):
+    try:
+      # Transcribe speech to text
+      client = OpenAI()
+      audio_file = open(self.filepath, 'rb')
+      transcript = client.audio.transcriptions.create(
+        model='whisper-1',
+        file=audio_file,
+        language='en'
+      )
+      self.original_answer = transcript.text
+
+      # Pass the text to completion API for revision
+      completion = client.chat.completions.create(
+        model='gpt-4',
+        messages=[
+          {
+            'role': 'system',
+            'content': f'You are a highly skilled AI that is trained in English speaking and eductaion. I would like you to read the following transcript which is my answer to this practice speaking test question of IELTS: {self.question_entry.get().strip()}. Please give me a revised and enhanced version of similar length. Please do not provide any other text in your response than the revised and enhanced answer.'
+          },
+          {
+            'role': 'user',
+            'content': self.original_answer
+          }
+        ]
+      )
+      self.revised_answer = completion.choices[0].message.content
+
+      # generate text file containing the two pieces of text
+      Path('outputs').mkdir(parents=True, exist_ok=True)
+      filepath = f'outputs/output.txt'
+      i = 1
+      while True:
+        if Path(filepath).exists():
+          filepath = f'outputs/output_{i}.txt'
+          i += 1
+        else:
+          break
+
+      formatted_text = f'''
+        Your answer:\n\n
+        {self.original_answer}\n\n
+        Revised answer:\n\n
+        {self.revised_answer}\n
+      '''
+
+      with open(filepath, 'w') as f:
+        f.write(formatted_text)
+
+      self._update_result_label()
+    except Exception as e:
+      messagebox.showerror(title='Error', message=f'{e}')
+
+  def _update_result_label(self):
+    self.result_label.configure(text='Done.')
+
+if __name__ == "__main__":
+  root = Tk()
+  app = SpeakingAssistant(root)
+  app.mainloop()
