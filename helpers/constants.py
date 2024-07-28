1cd2987d596e61b6c1a585eef00309a332e4727d
Jesse Wei <jesseweijapan@gmail.com>
2024-07-28 10:54:51 +0900

    allow user to select test part and store results in corresponding page

---
+++ helpers/constants.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NOTION_API_TOKEN = os.getenv('NOTION_API_TOKEN')
+URL_SEG1 = 'https://api.notion.com/v1/blocks/'
+URL_SEG2 = '/children'
+
+PARTS = {
+  'Part 1': os.getenv('PAGE_ID_PART1'),
+  'Part 2': os.getenv('PAGE_ID_PART2'),
+  'Part 3': os.getenv('PAGE_ID_PART3'),
+}
--- main.py
+++ main.py
@@ -3,60 +3,120 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import filedialog
 from tkinter import messagebox
-import os
-from dotenv import load_dotenv
 import re
 import helpers.notion_api as api
-
-load_dotenv()
-
-NOTION_API_TOKEN = os.getenv('NOTION_API_TOKEN')
-PAGE_ID = os.getenv('PAGE_ID')
-URL = f'https://api.notion.com/v1/blocks/{PAGE_ID}/children'
+from helpers.constants import *
 
 class SpeakingAssistant(Frame):
   def __init__(self, root):
     super().__init__(root)
+
+    style = Style()
+    style.configure('Alert.TLabel', foreground='red')
+
     self.root = root
     self.root.title = 'Speaking Assistant'
     self.filepath = None
     self.original_answer = None
     self.revised_answer = None
+    self.test_part = ''
     self.question = ''
 
     self.main_frame = Frame(self.root)
     self.main_frame.grid(padx=20, pady=20, sticky='we')
-    self.main_frame.columnconfigure(0, minsize=100, weight=1)
-    self.main_frame.columnconfigure(1, minsize=100, weight=1)
-    self.main_frame.columnconfigure(2, minsize=100, weight=1)
-    self.main_frame.columnconfigure(3, minsize=100, weight=1)
 
-    Label(self.main_frame, text='Question:').grid(row=0, column=0, sticky=W, padx=5)
+    # select widgets group
+    self.select_frame = Frame(self.main_frame)
+    self.select_frame.grid(row=0, column=0, sticky='we')
+    self.select_frame.columnconfigure(0, minsize=100, weight=1)
+    self.select_frame.columnconfigure(1, minsize=100, weight=1)
+    self.select_frame.columnconfigure(2, minsize=100, weight=1)
+    self.select_frame.columnconfigure(3, minsize=100, weight=1)
 
-    self.question_entry = Entry(self.main_frame)
+    Label(self.select_frame, text='Select test part:').grid(row=0, column=0, sticky=W, padx=5)
+
+    self.select = Combobox(self.select_frame, values=list(PARTS.keys()))
+    self.select.grid(row=1, column=0, columnspan=4, sticky='we', padx=5)
+    self.select.bind('<<ComboboxSelected>>', self._on_select)
+
+    self.alert_msg_test_part = Label(self.select_frame, text='You must choose a part.', style='Alert.TLabel')
+
+    # question entry widgets group
+    self.question_frame = Frame(self.main_frame)
+    self.question_frame.grid(row=1, column=0, sticky='we')
+    self.question_frame.columnconfigure(0, minsize=100, weight=1)
+    self.question_frame.columnconfigure(1, minsize=100, weight=1)
+    self.question_frame.columnconfigure(2, minsize=100, weight=1)
+    self.question_frame.columnconfigure(3, minsize=100, weight=1)
+
+    Label(self.question_frame, text='Question:').grid(row=0, column=0, sticky=W, padx=5)
+
+    self.question_entry = Entry(self.question_frame)
     self.question_entry.grid(row=1, column=0, columnspan=4, sticky='we', padx=5)
+    self.question_entry.bind('<KeyPress>', self._on_key_press)
 
-    self.filepath_btn = Button(self.main_frame, text='Select Audio File', command=self.open_file_browser)
-    self.filepath_btn.grid(row=2, column=0, columnspan=4, sticky='we', padx=5)
+    self.alert_msg_question = Label(self.question_frame, text='You must enter a question.', style='Alert.TLabel')
 
-    self.filepath_label = Label(self.main_frame, text='')
-    self.filepath_label.grid(row=3, column=0, columnspan=4, padx=5)
+    # file select widgets group
+    self.file_select_frame = Frame(self.main_frame)
+    self.file_select_frame.grid(row=2, column=0, sticky='we')
+    self.file_select_frame.columnconfigure(0, minsize=100, weight=1)
+    self.file_select_frame.columnconfigure(1, minsize=100, weight=1)
+    self.file_select_frame.columnconfigure(2, minsize=100, weight=1)
+    self.file_select_frame.columnconfigure(3, minsize=100, weight=1)
 
-    self.gen_btn = Button(self.main_frame, text='Generate Text', command=self.generate_text)
-    self.gen_btn.grid(row=4, column=0, columnspan=2, sticky='we', padx=5)
+    self.filepath_btn = Button(self.file_select_frame, text='Select Audio File', command=self.open_file_browser)
+    self.filepath_btn.grid(row=0, column=0, columnspan=4, sticky='we', padx=5)
 
-    self.quit_btn = Button(self.main_frame, text='Quit', command=lambda: self.root.quit())
-    self.quit_btn.grid(row=4, column=2, columnspan=2, sticky='we', padx=5)
+    self.filepath_label = Label(self.file_select_frame, text='')
+    self.filepath_label.grid(row=1, column=0, columnspan=4, padx=5)
+
+    self.alert_msg_file = Label(self.file_select_frame, text='You must choose a audio file.', style='Alert.TLabel')
+
+    # button widgets group
+    self.buttons_frame = Frame(self.main_frame)
+    self.buttons_frame.grid(row=3, column=0, sticky='we')
+    self.buttons_frame.columnconfigure(0, minsize=100, weight=1)
+    self.buttons_frame.columnconfigure(1, minsize=100, weight=1)
+    self.buttons_frame.columnconfigure(2, minsize=100, weight=1)
+    self.buttons_frame.columnconfigure(3, minsize=100, weight=1)
+
+    self.gen_btn = Button(self.buttons_frame, text='Generate Text', command=self.generate_text)
+    self.gen_btn.grid(row=0, column=0, columnspan=2, sticky='we', padx=5)
+
+    self.quit_btn = Button(self.buttons_frame, text='Quit', command=lambda: self.root.quit())
+    self.quit_btn.grid(row=0, column=2, columnspan=2, sticky='we', padx=5)
+
+  def _show_alert_msg(self, msg_widget, row_index, col_index):
+    msg_widget.grid(row=row_index, column=col_index, sticky=W, padx=5)
+
+  def _on_select(self, event):
+    self.alert_msg_test_part.grid_forget()
+
+  def _on_key_press(self, event):
+    self.alert_msg_question.grid_forget()
 
   def open_file_browser(self):
+    self.alert_msg_file.grid_forget()
+
     self.filepath = filedialog.askopenfilename()
     if not self.filepath:
+      self._show_alert_msg(self.alert_msg_file, 2, 0)
       return
     self.filepath_label.configure(text=self.filepath)
 
   def generate_text(self):
+    self.test_part = self.select.get()
+    if not self.test_part:
+      self._show_alert_msg(self.alert_msg_test_part, 2, 0)
+      return
+
+    self.question = self.question_entry.get().strip()
+    if not self.question:
+      self._show_alert_msg(self.alert_msg_question, 2, 0)
+      return
+
     try:
-      self.question = self.question_entry.get().strip()
       # Transcribe speech to text
       client = OpenAI()
       audio_file = open(self.filepath, 'rb')
@@ -93,7 +153,8 @@ class SpeakingAssistant(Frame):
         ]
       }
 
-      api.send_patch_request(URL, NOTION_API_TOKEN, data)
+      url = URL_SEG1 + PARTS[self.test_part] + URL_SEG2
+      api.send_patch_request(url, NOTION_API_TOKEN, data)
       messagebox.showinfo(title="Done", message="Task succeeded.")
     except Exception as e:
       messagebox.showerror(title='Error', message=f'{e}')
